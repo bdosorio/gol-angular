585208603bbde968f70e82a5277a36726ca07602
bdosorio <contact@brianosorio.com>
2014-03-22 11:05:25 -0500

    add intial step, keepRange, lastId.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,6 +4,11 @@ angular.module('golAngularApp.controllers')
     .controller('MainCtrl', function ($scope, $state) {
         var MAX_ID = 100, MAX_X = 500, MAX_Y = 500, MAX_AGE = 100, MAX_INIT_CELLS = 10, MAX_MUTATION = 10;
         $scope.cellList = [];
+        $scope.keepRange = {
+            start: 2,
+            end: 3
+        };
+        $scope.lastId = 0;
         $scope.generateRandomCell = function () {
             return {
                 id: $scope.generateRandomNumber(0, MAX_ID),
@@ -29,17 +34,67 @@ angular.module('golAngularApp.controllers')
             console.debug($state.current.name);
         };
         $scope.cellAt = function (x, y) {
-            var found;
+            var found = false;
             angular.forEach($scope.cellList, function (cell) {
                 if (cell.x === x && cell.y === y) {
                     found = cell;
                 }
             });
-            //noinspection JSUnusedAssignment
             return found;
         };
         $scope.addCell = function (cell) {
+            cell.id = $scope.lastId + 1;
+            $scope.lastId = cell.id;
             $scope.cellList.push(cell);
         };
-        $scope.step = function(){};
+        $scope.neighborCount = function (cell) {
+            var count = 0;
+            //top left
+            if ($scope.cellAt(cell.x - 1, cell.y - 1)) {
+                count++;
+            }
+            //top
+            if ($scope.cellAt(cell.x, cell.y - 1)) {
+                count++;
+            }
+            //top right
+            if ($scope.cellAt(cell.x + 1, cell.y - 1)) {
+                count++;
+            }
+            //left
+            if ($scope.cellAt(cell.x - 1, cell.y)) {
+                count++;
+            }
+            //right
+            if ($scope.cellAt(cell.x + 1, cell.y)) {
+                count++;
+            }
+            //bottom left
+            if ($scope.cellAt(cell.x - 1, cell.y + 1)) {
+                count++;
+            }
+            //bottom
+            if ($scope.cellAt(cell.x, cell.y + 1)) {
+                count++;
+            }
+            //bottom right
+            if ($scope.cellAt(cell.x + 1, cell.y + 1)) {
+                count++;
+            }
+            return count;
+        };
+        $scope.step = function () {
+            var removeList = [];
+            angular.forEach($scope.cellList, function (cell) {
+                var neighborCount = $scope.neighborCount(cell);
+                if (neighborCount < $scope.keepRange.start || neighborCount > $scope.keepRange.end) {
+                    removeList.push(cell);
+                }
+            });
+
+            angular.forEach(removeList, function (cell) {
+                $scope.cellList.splice($scope.cellList.indexOf(cell), 1);
+            });
+
+        };
     });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -13,7 +13,16 @@ describe('Controller: MainCtrl', function () {
     }));
 
     var MainCtrl,
-        scope, state;
+        scope, state,
+        cell = {x: 1, y: 1},
+        topLeft = {x: 0, y: 0},
+        top = {x: 1, y: 0},
+        topRight = {x: 2, y: 0},
+        left = {x: 0, y: 1},
+        right = {x: 2, y: 1},
+        bottomLeft = {x: 0, y: 2},
+        bottom = {x: 1, y: 2},
+        bottomRight = {x: 2, y: 2};
 
     // Initialize the controller and a mock scope
     beforeEach(inject(function ($controller, $rootScope, $injector) {
@@ -46,19 +55,18 @@ describe('Controller: MainCtrl', function () {
     it("should have a method to generate a cell", function () {
         expect(scope.generateRandomCell).toBeDefined();
 
-        var cell = scope.generateRandomCell();
+        var random = scope.generateRandomCell();
 
-        expect(cell.x).toBeDefined();
-        expect(cell.y).toBeDefined();
-        expect(cell.id).toBeDefined();
-        expect(cell.age).toBeDefined();
-        expect(cell.value).toBeDefined();
+        expect(random.x).toBeDefined();
+        expect(random.y).toBeDefined();
+        expect(random.id).toBeDefined();
+        expect(random.age).toBeDefined();
+        expect(random.value).toBeDefined();
     });
 
     it("should have a method to add a cell to the list", function () {
         expect(scope.addCell).toBeDefined();
 
-        var cell = scope.generateRandomCell();
         scope.addCell(cell);
 
         expect(scope.cellList[0]).toBe(cell);
@@ -68,23 +76,95 @@ describe('Controller: MainCtrl', function () {
     it("should have a method to determine if a cell exists at a grid position", function () {
         expect(scope.cellAt).toBeDefined();
 
-        scope.addCell({
-            x: 1,
-            y: 1
-        });
+        scope.addCell(cell);
+
         var isCellAt = scope.cellAt(scope.cellList[0].x, scope.cellList[0].y);
 
         expect(isCellAt).toBeTruthy();
     });
 
+    it('should have a method to determine the neighbor count of a cell', function () {
+        expect(scope.neighborCount).toBeDefined();
+
+
+        scope.addCell(cell);
+        expect(scope.neighborCount(cell)).toBe(0);
+
+        scope.addCell(topLeft);
+        expect(scope.neighborCount(cell)).toBe(1);
+
+        scope.addCell(top);
+        expect(scope.neighborCount(cell)).toBe(2);
+
+        scope.addCell(topRight);
+        expect(scope.neighborCount(cell)).toBe(3);
+
+        scope.addCell(left);
+        expect(scope.neighborCount(cell)).toBe(4);
+
+        scope.addCell(right);
+        expect(scope.neighborCount(cell)).toBe(5);
+
+        scope.addCell(bottomLeft);
+        expect(scope.neighborCount(cell)).toBe(6);
+
+        scope.addCell(bottom);
+        expect(scope.neighborCount(cell)).toBe(7);
+
+        scope.addCell(bottomRight);
+        expect(scope.neighborCount(cell)).toBe(8);
+    });
+
     describe('will have a step method that', function () {
+        beforeEach(function () {
+            scope.keepRange = {
+                start: 2,
+                end: 3
+            };
+        });
+
+
         it('should do nothing with an empty list', function () {
             expect(scope.step).toBeDefined();
 
             scope.cellList = [];
             scope.step();
+
             expect(scope.cellList.length).toBe(0);
+        });
+
+        it('should kill a cell if it has a neighbor count below keep range', function () {
+            scope.addCell(cell);
+
+            scope.step();
+
+            expect(scope.cellAt(cell.x, cell.y)).toBeFalsy();
+        });
+
+        it('should keep cells alive if in keep range', function () {
+            scope.addCell(topRight);
+            scope.addCell(top);
+            scope.addCell(cell);
+
+            scope.step();
+
+            expect(scope.cellAt(topRight.x, topRight.y)).toBeTruthy();
+            expect(scope.cellAt(top.x, top.y)).toBeTruthy();
+            expect(scope.cellAt(cell.x, cell.y)).toBeTruthy();
+        });
+
+        it('should keep cells alive if over keep range', function () {
+            scope.addCell(topRight);
+            scope.addCell(bottomRight);
+            scope.addCell(topLeft);
+            scope.addCell(bottomLeft);
+            scope.addCell(cell);
+
+            scope.step();
+
+            expect(scope.cellAt(cell.x, cell.y)).toBeFalsy();
         });
     });
 
-});
+})
+;
