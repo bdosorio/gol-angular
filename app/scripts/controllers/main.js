708733f85c2f0c842cb2f9e2658a635f6f475660
bdosorio <contact@brianosorio.com>
2014-05-08 01:42:54 -0500

    add logic to add cell on click and move more things to settings

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,28 +2,29 @@
 
 angular.module('golAngularApp.controllers')
     .controller('MainCtrl', function ($scope, $state) {
-        var MAX_ID = 100, MAX_X = 100, MAX_Y = 100, MAX_AGE = 100, MAX_INIT_CELLS = 10, MAX_MUTATION = 10, POSSIBLE_NEIGHBORS = 8;
+        var MAX_ID = 100, MAX_INIT_AGE = 100, MAX_INIT_CELLS = 100, MAX_MUTATION = 250, POSSIBLE_NEIGHBORS = 8;
         $scope.cellList = [];
         $scope.settings = {
+            drawRadius: 2,
             fillColor: '#b007b0',
             strokeWidth: '.1',
             strokeColor: '#000',
-            radius: 2,
-            drawRadius: 3,
+            boardWidth: 100,
+            boardHeight: 100,
             opacity: 0.6,
             keepRange: {
                 start: 2,
                 end: 3
             },
-            parentsForBirth : 3
+            parentsForBirth: 3
         };
         $scope.lastId = 0;
         $scope.generateRandomCell = function () {
             return {
                 id: $scope.generateRandomNumber(0, MAX_ID),
-                x: $scope.generateRandomNumber(0, MAX_X),
-                y: $scope.generateRandomNumber(0, MAX_Y),
-                age: $scope.generateRandomNumber(0, MAX_AGE),
+                x: $scope.generateRandomNumber(0, $scope.settings.boardWidth),
+                y: $scope.generateRandomNumber(0, $scope.settings.boardHeight),
+                age: $scope.generateRandomNumber(0, MAX_INIT_AGE),
                 value: {
                     mutation: $scope.generateRandomNumber(0, MAX_MUTATION)
                 }
@@ -53,8 +54,8 @@ angular.module('golAngularApp.controllers')
             cell.id = $scope.lastId + 1;
             $scope.lastId = cell.id;
             cell.age = cell.age || 0;
-            cell.drawX = (cell.x * $scope.settings.radius * 2) + $scope.settings.radius;
-            cell.drawY = (cell.y * $scope.settings.radius * 2) + $scope.settings.radius;
+            cell.drawX = cell.x * 2 * $scope.settings.drawRadius;
+            cell.drawY = cell.y * 2 * $scope.settings.drawRadius;
             $scope.cellList.push(cell);
         };
         $scope.findNeighborCount = function (cell, deadNeighborLength) {
@@ -117,14 +118,28 @@ angular.module('golAngularApp.controllers')
             $scope.cellList = _.difference($scope.cellList, removeList);
 
             //check possibleBirth to see if any need to be born
-            _.forEach(possibleBirthList,function(cell){
-                if($scope.findNeighborCount(cell) === $scope.settings.parentsForBirth){
+            _.forEach(possibleBirthList, function (cell) {
+                if ($scope.findNeighborCount(cell) === $scope.settings.parentsForBirth) {
                     $scope.addCell(cell);
                 }
-            } );
+            });
 
         };
-        $scope.clear= function(){
+        $scope.clear = function () {
             $scope.cellList = [];
+        };
+        $scope.svgClickHandler = function (clickEvent) {
+//            $scope.clickCell = _.pairs(clickEvent);
+            console.log(clickEvent);
+            var cell = {
+                x: Math.floor(clickEvent.offsetX / (2 * $scope.settings.drawRadius )),
+                y: Math.floor(clickEvent.offsetY / (2 * $scope.settings.drawRadius)),
+                value: {
+                    mutation: $scope.generateRandomNumber(0, MAX_MUTATION),
+                }
+            };
+            console.log("Adding:");
+            console.log(cell);
+            $scope.addCell(cell);
         };
     });
